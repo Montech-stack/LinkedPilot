feaaf6472bf7629b6037a9e1cacb12093eebd6d8
Montech-stack <montechverse@gmail.com>
2025-12-12 08:34:00 +0100

    ra 3

--- app/api/run-automations/route.ts
+++ app/api/run-automations/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from "next/server";
 import Automation from "@/models/Automation";
 import SocialAccount from "@/models/SocialAccount";
