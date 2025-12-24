3bc1a83842b7ddbd530003621363a4aab7e46179
Montech-stack <montechverse@gmail.com>
2025-12-24 23:18:05 +0100

    mc

--- app/page.tsx
+++ app/page.tsx
@@ -133,7 +133,7 @@ export default function LandingPage() {
                   color: "#1a1a1a",
                 }}
               >
-                Generate My First Post Free
+                Generate My First Post 
                 <ArrowRight className="ml-2 w-5 h-5" />
               </Button>
 
